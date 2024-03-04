5291f1b2572a347e9c6c6416008a0fbbbc74824d
Ali <2155399@roc-dev.com>
2024-03-04 11:16:35 +0100

    score board
    score board toegevoeg en bus gefixt

--- Les1JS/Les 1 JS/yahtzee.js
+++ Les1JS/Les 1 JS/yahtzee.js
@@ -2,108 +2,72 @@ const rollButton = document.getElementById("roll");
 const holdButton = document.getElementById("hold");
 
 
-const diceThrow = [1,2,3,4,5];
-let canThrow = [true,true,true,true,true];
 
-const dices = [0,0,0,0,0]
+let remainingTurns = 3;
 
-function diceRoll(dice){
-    if(dice < 0 || dice > 4){
+
+
+
+
+
+
+var heldDice = [false, false, false, false, false];
+
+function rollDice() {
+    var die1 = document.getElementById("dice1");
+    var die2 = document.getElementById("dice2");
+    var die3 = document.getElementById("dice3");
+    var die4 = document.getElementById("dice4");
+    var die5 = document.getElementById("dice5");
+    var total = document.getElementById("total");
+    var yat = document.getElementById("yahtzee");
+
+    if (!heldDice[0]) die1.innerHTML = Math.floor(Math.random() * 6) + 1;
+    if (!heldDice[1]) die2.innerHTML = Math.floor(Math.random() * 6) + 1;
+    if (!heldDice[2]) die3.innerHTML = Math.floor(Math.random() * 6) + 1;
+    if (!heldDice[3]) die4.innerHTML = Math.floor(Math.random() * 6) + 1;
+    if (!heldDice[4]) die5.innerHTML = Math.floor(Math.random() * 6) + 1;
+
+    var totaal = parseInt(die1.innerHTML) + parseInt(die2.innerHTML) + parseInt(die3.innerHTML) + parseInt(die4.innerHTML) + parseInt(die5.innerHTML);
+    total.innerHTML = totaal;
+
+    if (die1.innerHTML == die2.innerHTML && die1.innerHTML == die3.innerHTML && die1.innerHTML == die4.innerHTML && die1.innerHTML == die5.innerHTML) {
+        yat.innerHTML = "Yahtzee!";
+    } else {
+        yat.innerHTML = "No Yahtzee";
+    }
+
+
+
+
+    if (remainingTurns <= 0) {
+        alert("No  more turns left!");
+        remainingTurns == 3;
         return;
     }
-    if(canThrow[dice]){
-        diceThrow[dice] = Math.floor(Math.random() * 6) + 1;
-    }
+
+    remainingTurns--;
+    document.getElementById("remainingTurns").textContent = "You have " + remainingTurns + " rolls";
 }
 
-class Dice{
-    constructor(){
-        this.diceThrow = 1;
-        this.canThrow = true;
-    }
-
-    roll(){
-        if(this.canThrow){
-            this.diceThrow = Math.floor(Math.random() * 6) + 1;
-        }
-    }
-}
-
-for(let i = 0; i < dices.length; i++){
-    dices[i] = new Dice();
-}
- 
-dices.forEach(dice => {
-    dice.roll();
-});
-
-
-function diceHold(dice){
-    dices[dice].canThrow = false;
-  
-
-
-
-}
-
-function test(){
-    document.getElementById("roll").innerHTML = "Test passed!";
-    console.log(dices[1]);
-}
-
-
-
-
-
-
-
-
-function rollDice(){
-
-    var die1 =document.getElementById("dice1");
-    var die2 =document.getElementById("dice2");
-    var die3 =document.getElementById("dice3");
-    var die4 =document.getElementById("dice4");
-    var die5 =document.getElementById("dice5");
-
-
-    /* totale punten laten zien */
-    var total = document.getElementById("total");
-    var yat = document.getElementById("yahtzee"); //voor wanneer je yahtzee krijgt
-
-    var diceOne = Math.floor(Math.random() * 6) +  1; 
-    var diceTwo = Math.floor(Math.random() * 6) +  1; 
-    var diceThree = Math.floor(Math.random() * 6) +  1; 
-    var diceFour = Math.floor(Math.random() * 6) +  1; 
-    var diceFive = Math.floor(Math.random() * 6) +  1; 
-
-
-    var totaal = diceOne + diceTwo+diceThree+diceFour+diceFive ;
-    total = totaal;
-    innerHTML=total;
-
-    if(diceOne == diceTwo && diceOne ==diceThree && diceOne == diceFour && diceOne == diceFive){
-        yat.innerHTML = "Yahtzee!"
-    }else{
-        yat.innerHTML ="No Yahtzee"
-    }
-
-
+function holdDie(dieNum, ali) {
+    heldDice[dieNum - 1] = !heldDice[dieNum - 1];
 
 
     
-    
-
-    
-    
-
     //waarde van de dubblestenen laten zien 
     die1.innerHTML = diceOne;
     die2.innerHTML = diceTwo;
     die3.innerHTML = diceThree;
     die4.innerHTML = diceFour;
     die5.innerHTML = diceFive;
+
 }
+
+
+
+
+
 
 
 
